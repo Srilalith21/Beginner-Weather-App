57761c69f77e75b581a1beff824ab1bea99c54dd
Srilalith <sri21@gmail.com>
2025-07-26 19:37:08 +0530

    Fix Bug

--- script.js
+++ script.js
@@ -9,26 +9,27 @@ let cityName = null;
 let errorData = null;
 let weatherData = null;
 
-function updateValues(){
-  mainDegree.innerHTML = parseInt(weatherData.main.temp - 273.15).toString() + "&deg;";
+function updateValues() {
+  mainDegree.innerHTML =
+    parseInt(weatherData.main.temp - 273.15).toString() + "&deg;";
   speed.innerHTML = weatherData.wind.speed.toString();
-  humidity.innerHTML = parseInt(weatherData.main.humidity).toString() + "&percnt;";
+  humidity.innerHTML =
+    parseInt(weatherData.main.humidity).toString() + "&percnt;";
   currentLocation.innerHTML = cityName.toUpperCase();
 }
 
 async function fetchData() {
-
-  function apiKey(){
+  function apiKey() {
     let value = "";
     let one = "79f435cb04069e03";
     let two = "f5fe4a04f0e7c3f3";
     let eCount = 0;
     let oCount = 0;
-    for(let i=0; i<32; i++){
-      if(i%2 == 0){
+    for (let i = 0; i < 32; i++) {
+      if (i % 2 == 0) {
         value += one[eCount];
         eCount++;
-      }else{
+      } else {
         value += two[oCount];
         oCount++;
       }
@@ -36,18 +37,20 @@ async function fetchData() {
     return value;
   }
 
-  try{
+  try {
     searchButton.disabled = true;
-    searchButton.innerHTML = "searching..."
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey()}`)
+    searchButton.innerHTML = "searching...";
+    const data = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey()}`
+    );
     weatherData = await data.json();
     updateValues();
-    searchButton.innerHTML = "Search"
+    searchButton.innerHTML = "Search";
     searchButton.disabled = false;
-  }catch(error){
-    searchButton.innerHTML = "Search"
+  } catch (error) {
+    searchButton.innerHTML = "Search";
     searchButton.disabled = false;
-    alert("Invalid")
+    alert("Invalid City Name");
   }
 }
 
